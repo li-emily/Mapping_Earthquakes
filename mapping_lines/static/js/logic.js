b801b0535fb6d85ead005299ad4f5a6a48c6e6f8
Emily Li <emily.li@rutgers.edu>
2021-11-04 17:05:30 -0400

    Mapping lines.

---
+++ mapping_lines/static/js/logic.js
@@ -0,0 +1,43 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// Create the map object with a center and zoom level.
+let map = L.map("mapid").setView([30.194444, -97.67], 4);
+
+// // Coordinates for each point to be used in the polyline.
+// let line = [
+//   [33.9416, -118.4085],
+//   [37.6213, -122.3790],
+//   [40.7899, -111.9791],
+//   [47.4502, -122.3088]
+// ];
+
+// // Create a polyline using the line coordinates and make the line black.
+// L.polyline(line, {
+//   color: "yellow"
+// }).addTo(map);
+
+// Coordinates for airports
+let airports = [
+  [37.615223, -122.389977],
+  [30.194444, -97.67],
+  [43.6775, -79.63083333],
+  [40.641766, -73.780968]
+];
+
+// Create polyline for airport coordinates, make line dashed blue
+L.polyline(airports, {
+  color: "blue"
+}).addTo(map);
+
+  // We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/light-v10',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: API_KEY
+});
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
