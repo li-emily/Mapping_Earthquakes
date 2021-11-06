d731b5a9cca3b44d80688db587cc853f88865de7
Emily Li <emily.li@rutgers.edu>
2021-11-06 15:26:41 -0400

    Sorting module practice files.

--- Module_Practice_Files/mapping_geoJSON_points/static/js/logic.js
+++ Module_Practice_Files/mapping_geoJSON_points/static/js/logic.js
@@ -32,7 +32,7 @@ let map = L.map('mapid', {
 L.control.layers(baseMaps).addTo(map);
 
 // Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/li-emily/Mapping_Earthquakes/main/majorAirports.json";
+let airportData = "https://raw.githubusercontent.com/li-emily/Mapping_Earthquakes/main/Module_Practice_Files/majorAirports.json";
 
 // Grabbing our GeoJSON data.
 d3.json(airportData).then(function(data) {
